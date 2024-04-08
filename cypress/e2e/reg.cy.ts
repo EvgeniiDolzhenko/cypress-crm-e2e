65fb907b92b578247575a552732f41562c128e07
EvgeniiDolzhenko <zhrobot@gmail.com>
2024-04-07 22:13:42 -0400

    CB-005

--- cypress/e2e/reg.cy.ts
+++ cypress/e2e/reg.cy.ts
@@ -1,7 +1,7 @@
 import {businessUser} from '../support/helper'
 import {regPage} from "../pages/reg.page";
 
-describe('CB-004 Reg page input validation',()=>{
+describe('CB-004 Reg page inputs validation',()=>{
     it('Verify inputs', () => {
         cy.visit(businessUser.regPage)
         regPage.loginCompanyName.should('exist')
@@ -9,5 +9,17 @@ describe('CB-004 Reg page input validation',()=>{
         regPage.regLastName.should('exist')
         regPage.regLoginName.should('exist')
         regPage.regPass.should('exist')
+    });
+})
+
+describe('CB-005 Reg page inputs error validation',()=>{
+    it('Verify error message', () => {
+        cy.visit(businessUser.regPage)
+        regPage.loginCompanyName.should('exist')
+            .type('Company Name')
+            .clear()
+        cy.contains('div','Required')
+            .should('exist')
+            .and('have.css','color','rgb(255, 77, 79)')
     });
 })

0fdd8095473c40e0f37920cb148b4c6643bea9b1
Eugene <zhrobot@gmail.com>
2024-04-10 16:51:42 -0400

    CB-008/Delete vendor UI

--- cypress/e2e/vendors.cy.ts
+++ cypress/e2e/vendors.cy.ts
@@ -16,7 +16,7 @@ describe('CB-008 Create vendor',()=>{
         window.localStorage.setItem('token',Cypress.env('token'))
     })
 
-    it('Verify new vendor is created', () => {
+    it('Verify new vendor is created', function ()  {
         navbar.openBasePage()
         cy.intercept('POST','**/vendor/search').as('vendor')
         navbar.vendors.should('exist')
@@ -35,9 +35,32 @@ describe('CB-008 Create vendor',()=>{
             expect(response.statusCode).eq(200)
         })
         cy.get('@newVendor').then((newVendor: any)=>{
+            cy.wrap(newVendor.response.body.payload).as('id')
             vendorId = newVendor.response.body.payload
             cy.get(`a[href="/v5/vendor/${vendorId}"]`).should('exist')
                 .and('have.length',1)
         })
     });
+
+    after('Delete vendor UI',function (){
+        const id = this.id
+        window.localStorage.setItem('token',Cypress.env('token'))
+        navbar.openBasePage()
+        cy.intercept('POST','**/vendor/search').as('vendor')
+        navbar.vendors.should('exist')
+            .click()
+        cy.wait('@vendor')
+        cy.get(`[data-row-key="${id}"]`).should('exist')
+            .find('#top-menu img').trigger('mouseover')
+        cy.contains('span','Delete').click()
+        cy.get('[class="ant-modal-content"]').should('exist')
+        cy.intercept('DELETE',`**/${id}`).as('deleted')
+        cy.get('[class="ant-btn ant-btn-default ant-btn-dangerous"]').should('exist')
+            .and('have.css','border-bottom-color','rgb(255, 120, 117)')
+            .click()
+        cy.wait('@deleted').its('response').then((response)=>{
+            expect(response.statusCode).eq(200)
+        })
+
+    })
 })

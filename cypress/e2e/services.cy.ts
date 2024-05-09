ee0fffbb5c53a5897db442f63540a60ec794e4fe
Eugene <zhrobot@gmail.com>
2024-05-09 09:05:00 -0400

    service page

--- cypress/e2e/services.cy.ts
+++ cypress/e2e/services.cy.ts
@@ -1,12 +1,12 @@
 import {businessUser} from '../support/helper'
 import {loginPage} from "../pages/login.page";
-
+import { faker } from '@faker-js/faker';
 import { servicesPage } from '../pages/services.page';
 import {navbar} from "../pages/navbar";
 
-
-
 describe('CB-014 Verify new Service e2e',()=>{
+    const randomName = faker.person.fullName()
+    let serviceId : string
 
     before('Api login',()=>{
         loginPage.apiLogin(businessUser.apiUrl,businessUser.email,businessUser.pass)
@@ -16,17 +16,29 @@ describe('CB-014 Verify new Service e2e',()=>{
         window.localStorage.setItem('token',Cypress.env('token'))
     })
 
-
     it('Create new service UI', () => {
         navbar.openBasePage()
         navbar.openServices()
         cy.url().should('include','/service')
         servicesPage.createService.click()
-        servicesPage.description.type('something')
-        servicesPage.vendorName.click()
-        cy.get('[class="rc-virtual-list-holder-inner"] [class="ant-select-item-option-content"').should('exist')
+        servicesPage.serviceName.type(randomName)
+        servicesPage.description.type('Description')
+        cy.intercept('POST','**/vendor/search')
+        servicesPage.vendorName.click({force:true})
+        cy.get('[class="rc-virtual-list-holder-inner"] [class="ant-select-item-option-content"]').should('exist')
         cy.get('[class="rc-virtual-list-holder-inner"] [aria-selected="false"]').then((vendorList)=>{
-            cy.wrap(vendorList).eq(Cypress._.random(0,vendorList.length-1)).click()
+            cy.wrap(vendorList).eq(Cypress._.random(0,vendorList.length - 1)).click({force:true})
+        })
+        servicesPage.vendorPrice.type('150')
+        servicesPage.clientPrice.type('110')
+        cy.intercept('POST','**/service').as('newService')
+        servicesPage.createServiceButton.click()
+        cy.wait('@newService').its('response').then((response)=>{
+            expect(response.statusCode).eq(200)
+            expect(response.body.message).eq('Service created')
+            serviceId = response.body.payload
+
+            
         })
     });
 })
--- cypress/pages/services.page.ts
+++ cypress/pages/services.page.ts
@@ -5,15 +5,27 @@ class ServicesPage{
     }
 
     get serviceName(){
-        return cy.get('#name')
+        return cy.get('[class="ant-drawer-body"] #name')
     }
 
     get vendorName(){
-        return cy.get('.ant-select-in-form-item')
+        return cy.get('input[class="ant-select-selection-search-input"]')
     }
 
     get description(){
         return cy.get('#description')
+    }
+
+    get vendorPrice(){
+        return cy.get('#vendorPrice')
+    }
+
+    get clientPrice(){
+        return cy.get('#clientPrice')
+    }
+
+    get createServiceButton(){
+        return cy.get('[class="ant-drawer-wrapper-body"] [class="ant-btn ant-btn-primary"]')
     }
 
 
